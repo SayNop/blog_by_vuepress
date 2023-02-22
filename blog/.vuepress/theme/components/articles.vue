891c08ace0a382d6a3713a9fa8b58e8f2e702dd3
SayYep <b20457451@163.com>
2023-02-22 18:20:25 +0800

    update sidebar and statistical data

--- blog/.vuepress/theme/components/articles.vue
+++ blog/.vuepress/theme/components/articles.vue
@@ -1,20 +1,6 @@
 <template>
     <div id="article_list">
         <!-- <div>{{$site.pages}}</div>  -->
-        <div class="article_card card_border">
-            <div class="card_title_container">
-                <div class="card_title article_title">HelloWorld</div> 
-                <div class="article_time">2023-01-01 12:00:00</div>
-            </div>
-            <div class="card_content_container">
-                <div class="card_content">print('Hello World')</div>
-                <div class="card_tag">
-                    <span class="article_category">python</span>
-                    <span class="article_tag">python</span>
-                    <span class="article_tag">vue</span>
-                </div> 
-            </div>
-        </div>
         <div class="article_list" v-for="item in $site.pages" :key="item.key">
             <div class="article_card card_border" v-if="item.frontmatter.layout=='detail'" @click="go_to_detail(item.regularPath)">
                 <div class="card_title_container">
@@ -24,7 +10,6 @@
                 <div class="card_content_container">
                     <div class="card_content">{{ item.frontmatter.discription }}</div>
                     <div class="card_tag">
-                        <!-- <span class="article_category">{{ item.frontmatter.category }}</span> -->
                         <category_lable :datas="item.frontmatter.category"/>
                         <tag_lable :datas="tag"  v-for="tag in item.frontmatter.tags" :key="tag" />
                     </div> 

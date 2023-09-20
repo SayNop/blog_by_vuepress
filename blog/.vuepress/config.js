74ed3f9313cee474d805312916d0efc993c3d51c
SayNop <a2045751@163.com>
2023-09-20 10:22:02 +0800

    update markdown style

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     head: [
         [
             'link', // 设置 favicon.ico，注意图片放在 public 文件夹下
-            { rel: 'icon', href: 'favicon.ico' }
+            { rel: 'icon', href: '/favicon.ico' }
         ]
     ],
     plugins: [
